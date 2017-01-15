e3e7f3ffbe8086f574cf1fc3c42463a99738b51a
sho <sho_ws@outlook.com>
2017-01-16 01:09:30 +0900

    sigeru

--- app_main.rb
+++ app_main.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'line/bot'
-
+require './messages'
 get '/' do
-  "Hello world"
+	reply_template_museum(reply_data).to_s
 end
 
 def client
-  @client ||= Line::Bot::Client.new { |config|
+	@client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
@@ -26,11 +26,11 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
+        if event.message['text'] =~ /情報/
+	        client.reply_message(event['replyToken'], reply_template_museum(reply_data))
+        else
+	        client.reply_message(event['replyToken'], reply_message(event.message['text']))
+  			end
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
