e1028462fa567765d170baf39d523d8b62937481
sho <sho_ws@outlook.com>
2017-01-16 10:27:48 +0900

    postback

--- app_main.rb
+++ app_main.rb
@@ -38,7 +38,7 @@ post '/callback' do
       end
     # Postbackの場合
     when Line::Bot::Event::Postback
-      if event["postback"]["label"] =~ /Keep/
+      if event["postback"]["data"] =~ /keep/
         client.reply_message(event['replyToken'], reply_message(event["postback"]["data"]))
       end
     end
